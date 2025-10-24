d1b1e32a5bca45dc4c02b1685e23f82f7869f6c1
amit kumar gupta <amitgupta.gwl@gmail.com>
2025-10-24 12:11:51 +0530

    fix logo height

--- assets/js/components.js
+++ assets/js/components.js
@@ -5,7 +5,7 @@ const Header = () => `
     <nav class="navbar navbar-expand-lg navbar-light">
         <div class="container">
             <a class="navbar-brand" href="/">
-              <img src="/assets/images/logo.png" alt="SoloThought">
+              <img src="/assets/images/logo.png" alt="SoloThought" style="height: 40px;">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
